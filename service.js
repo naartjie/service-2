1b637af0c67dc9c66a2ca92d2f88c398e8b9e5aa
Marcin Jekot <marcin@jekot.net>
2016-05-17 11:40:05 +0200

    echo last 5 chars of fugly docker hostname

--- service.js
+++ service.js
@@ -7,13 +7,13 @@ const app = express()
 app.route('/health').get((req, res) => res.send('ok'))
 
 app.use((req, res) => {
-  console.log(`service-2: ${req.method} ${req.url}`)
-  exec('/sbin/ifconfig eth0 | grep \'inet addr:\' | cut -d: -f2 | awk \'{ print $1}\'', (err, stdout) => {
-    const out = `service-2: ${(new Date()).toString()} on ${(err || stdout)}`
+  console.log(`time-service: ${req.method} ${req.url}`)
+  exec('hostname', (err, stdout) => {
+    const out = `time-service: ${(new Date()).toString()} on host "${(err || stdout).trim().slice(-5)}"\n`
     res.send(out)
   })
 })
 
 app.listen(3000, () => {
-  console.log('service-2 started on port 3000')
+  console.log('time-service started on port 3000')
 })
